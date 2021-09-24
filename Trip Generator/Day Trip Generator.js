1378115c71c144d911e15534c84e298afac41e3c
Ebony Riddick <ebonyriddick@gmail.com>
2021-09-24 12:40:30 -0400

    Init

---
+++ Trip Generator/Day Trip Generator.js
@@ -0,0 +1,32 @@
+let entertainment = ['Wine Tour', 'State Park', 'Brewery Tour', 'Festival'];
+let transportation = ["train", "Bus", "car"];
+let destination = ['South Carolina', 'Georgia', 'Alabama', 'Tennessee'];
+let resturant = ['Sea Food', 'BBQ', 'Southern Cuisine', 'Steakhouse'];
+let start = "go";
+
+let randomNumber = Math.floor(Math.random()*3);
+let randomNumber2 = Math.floor(Math.random()*4);
+
+
+
+function determineDestination(){
+    let response = destination[randomNumber2];
+    return response;
+}
+let place = determineDestination();
+
+function determineTransportation(){
+    let how = transportation[randomNumber];
+    return how;
+}
+let mode = determineTransportation();
+
+// function determineEntertainment(){
+//     console.log(entertainment[randomNumber]);
+//     return;
+// }
+
+// function determineResturant(){
+//     console.log(resturant[randomNumber2]);
+//     return;
+// }
